9bee5c6d34a060371b1fe74854b62b2fad8c59a3
catkhuu <sunshine.dumbo@gmail.com>
2016-09-26 17:09:10 -0400

    Create Room class.

--- room.rb
+++ room.rb
@@ -1,2 +1,10 @@
 class Room
+
+  attr_reader :sqft, :window
+  attr_accessor :name
+  def initialize(args={})
+    @name = args.fetch(:name, "bedroom")
+    @sqft = args.fetch(:sqft, 120)
+    @window = args.fetch(:window, 0)
+  end
 end
