2ff3eae9f8ecc726192f3d6891d015a48b12e3e6
Jesusmar Lima & Roman Levytskyy <pair+jesusmarlima.levytsroman@devbootcamp.com>
2016-09-26 19:31:25 -0400

    add code to tests pass

--- apartment.rb
+++ apartment.rb
@@ -1,2 +1,28 @@
 class Apartment
+  attr_reader :number, :rooms, :monthly_rent
+
+  def initialize(args={})
+    @number = args.fetch(:number,"")
+    @rooms = args.fetch(:rooms,[])
+    @monthly_rent = args.fetch(:monthly_rent,1000)
+  end
+
+  def total_sqft
+    rooms.reduce(0){| total_sqft, room | total_sqft + room.sqft }
+  end
+
+  def price_per_sqft
+      self.monthly_rent / total_sqft.to_f     
+  end
+
+  def room_count
+    self.rooms.length
+  end
+
+  def bedroom_count
+    self.rooms.count{|room| room.name == "bedroom" }
+  end
+
+
+
 end
