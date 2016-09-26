3c87e7fa15e0cfb7c12d8deb2c1792628792a46f
[01;31m[K"name"[m[K: "Catherine Khuu", &   [01;31m[K"name"[m[K: "Nan Min", &   [01;31m[K"name"[m[K: "Wellington Vicioso <pair+catkhuu.nanutza.wvicioso@devbootcamp.com>
2016-09-26 18:21:30 -0400

    Create Class Room, Apartment and Building and attributes - passed all RSPEC

--- apartment.rb
+++ apartment.rb
@@ -1,2 +1,30 @@
 class Apartment
+	attr_reader :number, :monthly_rent, :rooms
+
+	def initialize( args = {} )
+		@number = args.fetch(:number,'')
+		@rooms = args.fetch(:rooms, [])
+		@monthly_rent = args.fetch(:monthly_rent, 1000)
+	end
+
+	# def generate_rooms(room)
+	# 	rooms << room
+	# end
+
+	def total_sqft
+		rooms.reduce(0) { |total, room| total + room.sqft}
+	end
+
+	def price_per_sqft
+		monthly_rent / total_sqft.to_f
+	end
+
+	def room_count
+		rooms.length
+	end
+	def bedroom_count
+		rooms.count { |room| room.name == 'bedroom'}
+	end
 end
+
+
