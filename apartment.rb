81a450a9e0d302666f1e2739c8844d38c355e1f0
catkhuu <sunshine.dumbo@gmail.com>
2016-09-27 16:14:55 -0400

    Create methods for Apartment class.

--- apartment.rb
+++ apartment.rb
@@ -1,2 +1,24 @@
 class Apartment
+  attr_reader :rooms, :monthly_rent
+  def initialize(args={})
+    @number = args.fetch(:number, "")
+    @monthly_rent = args.fetch(:monthly_rent, 1000)
+    @rooms = args.fetch(:rooms, [])
+  end
+
+  def total_sqft
+    rooms.inject(0) { |sum, room| sum + room.sqft }
+  end
+
+  def price_per_sqft
+    monthly_rent/total_sqft.to_f
+  end
+
+  def room_count
+    rooms.count
+  end
+
+  def bedroom_count
+    rooms.count { |room| room.name == "bedroom" }
+  end
 end
