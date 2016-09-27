4c3347749f95d659539adadf22889b6e6ce1f671
Matt Critelli <matthew.critelli@gmail.com>
2016-09-26 20:12:39 -0400

    Add initial class declarations for room, apartment, and building.

--- apartment.rb
+++ apartment.rb
@@ -1,2 +1,25 @@
+require_relative 'room'
+
 class Apartment
+
+  attr_reader :monthly_rent, :rooms
+
+  def initialize(args)
+    @number = args.fetch(:number)
+    @rooms = args.fetch(:rooms)
+    @monthly_rent = args.fetch(:monthly_rent, 1000)
+  end
+
+  def total_sqft
+  end
+
+  def price_per_sqft
+  end
+
+  def room_count
+  end
+
+  def bedroom_count
+  end
+
 end
--- building.rb
+++ building.rb
@@ -1,2 +1,14 @@
 class Building
+
+  attr_reader :apartments
+
+  def initialize
+    @address = args.fetch(:address)
+    @apartments = args.fetch(:apartments)
+  end
+
+
+
+
+
 end
