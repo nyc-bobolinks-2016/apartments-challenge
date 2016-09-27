2869f623873592ccc90f902e2783e346faf3daf2
catkhuu <sunshine.dumbo@gmail.com>
2016-09-27 15:39:42 -0400

    Write methods for Room class.

--- room.rb
+++ room.rb
@@ -1,2 +1,9 @@
 class Room
+  attr_accessor :name
+  attr_reader :sqft
+  def initialize(args={})
+    @sqft = args.fetch(:sqft, 120)
+    @window_count = args.fetch(:window_count, 0)
+    @name = args.fetch(:name, "bedroom")
+  end
 end
